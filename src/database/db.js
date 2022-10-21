2749080e496ac91c691e4673d3e9fe62ddfd61cf
Abiodun Shittu <jerrywizklay1@gmail.com>
2022-10-21 21:54:07 +0100

    Remote Database

--- src/database/db.js
+++ src/database/db.js
@@ -5,12 +5,12 @@ dotenv.config();
 const { Pool } = pg;
 
 const pool = new Pool({
-	user: process.env.User,
-	host: process.env.Host,
-	database: process.env.Database,
-	password: process.env.Password,
-	port: process.env.Port,
-	connectionString: process.env.URI,
+	// user: process.env.User,
+	// host: process.env.Host,
+	// database: process.env.Database,
+	// password: process.env.Password,
+	// port: process.env.Port,
+	connectionString: process.env.DATABASE_URL,
 	ssl: {
 		rejectUnauthorized: false,
 	},
