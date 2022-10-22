02ee430ec3563d8cc8bf183eea49435a931e7d51
Abiodun Shittu <jerrywizklay1@gmail.com>
2022-10-22 10:07:50 +0100

    Fix Todo Query Bug

--- src/utils/todoQueries.js
+++ src/utils/todoQueries.js
@@ -1,6 +1,6 @@
 const createTodoQuery =
 	"INSERT INTO todos (id, user_id, title, description, due_date, created_at, updated_at) VALUES ($1, $2, $3, $4, $5, $6, $7)";
-const findTodoQuery = "SELECT * FROM todos WHERE user_id = $1";
+const findTodoQuery = "SELECT * FROM todos WHERE id = $1";
 const deleteTodoQuery = "DELETE FROM todos WHERE id = $1";
 
 export {createTodoQuery, findTodoQuery, deleteTodoQuery}
