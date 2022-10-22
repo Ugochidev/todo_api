147abbd88e33ea044396af8e67dbbd12becbebaf
Abiodun Shittu <jerrywizklay1@gmail.com>
2022-10-22 09:59:08 +0100

    Fix Query Bug

--- src/utils/userQueries.js
+++ src/utils/userQueries.js
@@ -1,5 +1,5 @@
 const findEmailQuery = "SELECT id, email, password FROM users WHERE email = $1";
 const newUserQuery =
-	"INSERT INTO users ( id, first_name, last_name, email, phone_number, password) VALUES ( $1, $2, $3, $4, $5, $6)";
+	"INSERT INTO users ( id, name, email, password) VALUES ( $1, $2, $3, $4, $5, $6)";
 
 export { findEmailQuery, newUserQuery };
